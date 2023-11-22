1dc05988976d99e5f83ddb8b417c6129c1d38c75
Blasnjacobo <blasnjacobo05@gmail.com>
2023-11-22 12:13:04 -0700

    correcting home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <div>
       <h3>Hello, </h3>
-      <h5>We are still working in the design of this main page, and adding more collections. In the meantime, check out our <span style={{color:'red'}}> Harry Potter </span> and <span style={{color:'blue'}}> Avengers</span> collection</h5>
+      <h5>We are still working on the design of this main page, and adding more collections. In the meantime, check out our <span style={{color:'red'}}> Harry Potter </span> and <span style={{color:'blue'}}> Avengers</span> collection</h5>
     </div>
   )
 }
