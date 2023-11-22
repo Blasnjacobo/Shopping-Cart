9f93d1df441264a4b8ebdde653d5bb3638adac61
Blasnjacobo <blasnjacobo05@gmail.com>
2023-11-22 00:42:07 -0700

    basic structure complited

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Home from './pages/Home'
+import Store from './pages/Store'
+import About from './pages/About'
+import Navbar from './components/Navbar'
+import { ShoppingCartProvider } from './context/ShoppingCartContext'
 
 function App() {
-
   return (
-  <h1>React App</h1>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container  className='mb-4'>
+        <Routes>
+          <Route path='/shopping-cart' element={<Home />} />
+          <Route path='/shopping-cart/store' element={<Store />} />
+          <Route path='/shopping-cart/about' element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   )
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h3>Hello, </h3>
+      <h5>We are still working in the design of this main page, and adding more collections. In the meantime, check out our <span style={{color:'red'}}> Harry Potter </span> and <span style={{color:'blue'}}> Avengers</span> collection</h5>
+    </div>
+  )
+}
+
+export default Home
