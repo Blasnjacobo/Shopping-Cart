b5eaee13b9b51d499ba3a914997ae67a3fa4b546
Blasnjacobo <blasnjacobo05@gmail.com>
2024-02-19 15:18:32 -0700

    Perfume Shop3

--- src/pages/BreakingBad.tsx
+++ src/pages/BreakingBad.tsx
@@ -1,17 +1,17 @@
 import { Row, Col } from 'react-bootstrap'
-import breakingBad from '../data/breakingBad.json'
+import storeItems from '../data/items.json'
 import StoreItem from '../components/StoreItem'
 
 const BreakingBad = () => {
   return (
     <div>
-      <h1 style={{marginLeft:'1rem', textAlign:"center"}}>- Breaking Bad -</h1>
-      <iframe width="97%" height="315" 
-      src="https://www.youtube.com/embed/njtfNPB-YKg?si=Ip_rQSXWwzajlWQm" 
-      style={{display: 'block', margin:' 2rem auto'}}/>
-      <h2 style={{marginLeft:'1rem'}}>Trending products</h2>
+      <h1 style={{ marginLeft: '1rem', textAlign: "center" }}>- Breaking Bad -</h1>
+      <iframe width="97%" height="315"
+        src="https://www.youtube.com/embed/njtfNPB-YKg?si=Ip_rQSXWwzajlWQm"
+        style={{ display: 'block', margin: ' 2rem auto' }} />
+      <h2 style={{ marginLeft: '1rem' }}>Trending products</h2>
       <Row xs={1} md={2} lg={3} className='g-3'>
-        {breakingBad.map(item => (
+        {storeItems.map(item => (
           <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -1,17 +1,17 @@
 import { Row, Col } from 'react-bootstrap'
-import friends from '../data/friends.json'
+import storeItems from '../data/items.json'
 import StoreItem from '../components/StoreItem'
 
 const Friends = () => {
   return (
     <div>
-      <h1 style={{marginLeft:'1rem', textAlign:"center"}}>- Friends -</h1>
-      <iframe width="97%" height="315" 
-      src="https://www.youtube.com/embed/J3W4dz92WTI?si=3t10JBaB4budLzjC" 
-      style={{display: 'block', margin:' 2rem auto'}} />
-      <h2 style={{marginLeft:'1rem'}}>From boardgames to apparels</h2>
+      <h1 style={{ marginLeft: '1rem', textAlign: "center" }}>- Friends -</h1>
+      <iframe width="97%" height="315"
+        src="https://www.youtube.com/embed/J3W4dz92WTI?si=3t10JBaB4budLzjC"
+        style={{ display: 'block', margin: ' 2rem auto' }} />
+      <h2 style={{ marginLeft: '1rem' }}>From boardgames to apparels</h2>
       <Row xs={1} md={2} lg={3} className='g-3'>
-        {friends.map(item => (
+        {storeItems.map(item => (
           <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
