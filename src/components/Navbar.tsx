a2506ef2bd1b09cdc708cff813a84f359f10420e
Blasnjacobo <blasnjacobo05@gmail.com>
2024-02-19 19:08:02 -0700

    Perfume Shop6

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { useShoppingCart } from '../context/ShoppingCartContext';
