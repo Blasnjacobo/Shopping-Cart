3eab9b0da4cbaef2d8a783d8268a72295359411e
Blasnjacobo <blasnjacobo05@gmail.com>
2024-02-20 11:14:45 -0700

    others

--- src/pages/Caballero.tsx
+++ src/pages/Caballero.tsx
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap'
-import storeItems from '../data/items.json'
+import storeItems from '../data/friends.json'
 import StoreItem from '../components/StoreItem'
 
 const Caballero = () => {
--- src/pages/Unisex.tsx
+++ src/pages/Unisex.tsx
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap'
-import storeItems from '../data/items.json'
+import storeItems from '../data/starWars.json'
 import StoreItem from '../components/StoreItem'
 
 const Unisex = () => {
