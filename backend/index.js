68ab970878711e6b54528e91afa873ee04dfd966
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-01 13:18:53 -0700

    sessions back4

--- backend/index.js
+++ backend/index.js
@@ -13,12 +13,15 @@ const cart = require("./routes/cart.js");
 
 const app = express();
 
+const mongoStoreOptions = {
+  mongooseConnection: mongoose.connection,
+  collection: "sessions", // Optional: specify the name of the collection to store sessions
+};
+
 app.use(
   session({
     cookie: { maxAge: 86400000 },
-    store: new MemoryStore({
-      checkPeriod: 86400000, // prune expired entries every 24h
-    }),
+    store: new MongoStore(mongoStoreOptions), // Use new keyword when instantiating MongoStore
     resave: false,
     secret: process.env.SESSION_SECRET,
   })
@@ -48,7 +51,7 @@ mongoose
   .connect(process.env.mongoDBURL, {})
   .then(() => {
     app.listen(PORT, () =>
-      console.log(`Server Port: ${PORT}, you are connected to database`)
+      console.log(`Server Port: ${PORT}, you are connected to the database`)
     );
   })
   .catch((error) => console.log(`${error} did not connect`));
