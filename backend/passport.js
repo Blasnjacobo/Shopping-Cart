b0f5c04fdc4d9ba6084542f5be3b47c983d68045
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-02 15:50:13 -0700

    merge2

--- backend/passport.js
+++ backend/passport.js
@@ -10,7 +10,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL:
+        "https://shopping-cart-production-4ea1.up.railway.app/auth/google/callback",
     },
     async (request, accessToken, refreshToken, profile, done) => {
       try {
