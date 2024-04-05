4305dfcad01228f1e1d5165d85294d0921ff915d
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-05 10:33:23 -0700

    workaround 2

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -5,14 +5,14 @@ const increaseQuantity = require("../controllers/cart/increaseQuantity");
 const decreaseQuantity = require("../controllers/cart/decreaseQuantity");
 const removeFromCart = require("../controllers/cart/removeFromCart");
 const cartItems = require("../controllers/cart/cartItems");
-// const authenticateToken = require("../middleware/auth.js");
+const authenticateToken = require("../middleware/auth.js");
 
 const router = express.Router();
-router.get("/totalQuantity/:username", totalQuantity);
-router.get("/itemQuantity/:username/:_id", itemQuantity);
-router.post("/increase/:_id/:username", increaseQuantity);
-router.post("/decrease/:_id/:username", decreaseQuantity);
-router.delete("/delete/:_id/:username", removeFromCart);
-router.get("/:username", cartItems);
+router.get("/totalQuantity/:username", authenticateToken, totalQuantity);
+router.get("/itemQuantity/:username/:_id", authenticateToken, itemQuantity);
+router.post("/increase/:_id/:username", authenticateToken, increaseQuantity);
+router.post("/decrease/:_id/:username", authenticateToken, decreaseQuantity);
+router.delete("/delete/:_id/:username", authenticateToken, removeFromCart);
+router.get("/:username", authenticateToken, cartItems);
 
 module.exports = router;
